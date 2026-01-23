aeb444097a18b3f9bd7b2d5eef87c17be9525e5b
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-01-23 12:03:29 -0700

    Add files via upload

---
+++ MODEL/LIFE_STUDY_MODEL/BOOK_OF_ENOCH_MODEL/BOOK_OF_ENOCH_GET_MODEL/Book_Of_Enoch_Get_Model01.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace E_APP.MODEL.LIFE_STUDY_MODEL.BOOK_OF_ENOCH_MODEL.BOOK_OF_ENOCH_GET_MODEL
+{
+    internal class  Book_Of_Enoch_Get_Model01
+    {
+        public string thebook { get; set; }
+        public string bookname { get; set; }
+        public string charpter { get; set; }
+        public string verses { get; set; }
+    }
+}
