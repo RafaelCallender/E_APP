20fcff352d4ac48d2d199197fb7108fa25fab29e
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-01-23 12:04:08 -0700

    Add files via upload

---
+++ TEMPLATE/REGGEX_TEXT/Reggex_Text01.cs
@@ -0,0 +1,14 @@
+﻿namespace E_APP.TEMPLATE.REGGEX_TEXT
+{
+    internal class Reggex_Text01
+    {
+        private static string[] Regex01_ = { @"[^a-zA-Z0-9]",
+                                             @"^[^@\s]+@[^@\s]+\.[^@\s]+$"};
+
+        public static string[] Regex01
+        {
+            get { return Regex01_; }
+            set { Regex01_ = value; }
+        }
+    }
+}
