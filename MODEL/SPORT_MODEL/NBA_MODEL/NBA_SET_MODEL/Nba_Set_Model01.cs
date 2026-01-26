ec13782a16ae096db691e25c2b28e87f7e07c46e
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-01-26 07:53:38 -0700

    Add files via upload

--- MODEL/SPORT_MODEL/NBA_MODEL/NBA_SET_MODEL/Nba_Set_Model01.cs
+++ MODEL/SPORT_MODEL/NBA_MODEL/NBA_SET_MODEL/Nba_Set_Model01.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿
 
 namespace E_APP.MODEL.SPORT_MODEL.NBA_MODEL.NBA_SET_MODEL
 {
     internal class Nba_Set_Model01
     {
+        public string get { get; set; }
+        public object parameters { get; set; }
+        public object errors { get; set; }
+        public int results { get; set; }
+        public int response { get; set; }
     }
 }
