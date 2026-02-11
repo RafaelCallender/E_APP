b12689d5689c3c52178e509a194602b49e712a7e
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-10 21:40:30 -0700

    Add files via upload

---
+++ VIEW/MOVIES_VIEW/MOVIES_MAIN_VIEW/Movies_Main_View01.cs
@@ -0,0 +1,55 @@
+﻿using E_APP.SERVICES.SECURITY_SERVICES;
+using E_APP.VIEW.AI_VIEW.AI_SELECTION_VIEW;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_APP.VIEW.MOVIES_VIEW.MOVIES_MAIN_VIEW
+{
+    internal class Movies_Main_View01
+    {
+        private static string[] data01 = new string[100];
+        private static Security_Services01 Security_Serv01 = new Security_Services01();
+
+        public Movies_Main_View01()
+        {
+            load_Movies_Main_View01();
+        }
+        private void load_Movies_Main_View01()
+        {
+
+            data01[0] = $"1.) hairstyle\n";
+            Console.WriteLine(data01[0]);
+            data01[1] = Console.ReadLine() ?? string.Empty;
+
+
+            if (Security_Serv01.empty_string(data01[1]) == true)
+            {
+                if (Security_Serv01.string_only_digit(data01[1]) == true)
+                {
+                    switch (int.Parse(data01[1]))
+                    {
+                        case 1:
+                            new Ai_Selection_View01();
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Input must be only digits. Please try again.");
+                    data01[1] = Console.ReadLine() ?? string.Empty;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input cannot be empty. Please try again.");
+                data01[1] = Console.ReadLine() ?? string.Empty;
+
+            }
+
+
+        }
+
+    }
+}
