cff0ff851073100d232a757fbb211f833a2bb15e
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-20 13:19:51 -0700

    Add files via upload

---
+++ SERVICES/AI_SERVICES/AI_TEXT_TO_TEXT/Ai_Text_To_Text05.cs
@@ -0,0 +1,68 @@
+﻿using E_APP.SERVICES.FILE_SERVICES.FILE_HELPER;
+using LLama;
+using LLama.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using E_APP.SERVICES.AI_SERVICES.AI_HELPER;
+namespace E_APP.SERVICES.AI_SERVICES.AI_TEXT_TO_TEXT
+{
+    internal class Ai_Text_To_Text05
+    {
+        private readonly Dictionary<string, string> _chunkCache = new();
+        private static Ai_Helper01 Ai_H01 = new Ai_Helper01();
+        public Ai_Text_To_Text05()
+        {
+            Ai_H01.LoadModel();
+        }
+
+        public async Task<string> text_to_text_content01(string input, Action[] chunkLoader)
+        {
+
+            using var context = Ai_H01._model.CreateContext(Ai_H01._parameters);
+            var executor = new InteractiveExecutor(context);
+            string textfile_content = string.Empty;
+
+            for (int i = 0; i < chunkLoader.Length; i++)
+            {
+                if (!_chunkCache.TryGetValue(input, out textfile_content))
+                {
+                    textfile_content = Ai_H01.RetrieveContext(input, chunkLoader[i], maxChunks: 3);
+                    _chunkCache[input] = textfile_content;
+                }
+            }
+
+            string prompt = $"""
+
+You are a book of Enoch,The King James Bible, and book of the Jubilees scholar.
+Answer ONLY using the book of the Jubilees,book of Enoch, and The King James Bible text below.
+
+collection of book Text:
+{textfile_content}
+
+Question:
+{input}
+
+Answer:
+""";
+
+            var inferenceParams = new InferenceParams
+            {
+                MaxTokens = 512,
+
+            };
+
+            var result = new StringBuilder();
+
+            await foreach (var token in executor.InferAsync(prompt, inferenceParams))
+            {
+                result.Append(token);
+            }
+
+            return result.ToString().Trim();
+        }
+
+
+    }
+}
---
+++ SERVICES/SQL/SQL_SERVICES/SQL_LANGUAGE_SERVICES/Sql_Language_Services01.cs
@@ -0,0 +1,158 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+using E_APP.SERVICES.SQL_SERVICES.SQL.SQL_MANAGER.SQL_MANAGER_LANGUAGE;
+namespace E_APP.SERVICES.SQL.SQL_SERVICES.SQL_LANGUAGE_SERVICES
+{
+    internal class Sql_Language_Services01
+    {
+        private bool status = false;
+        private static string[] data01 = new string[3];
+        private List<string> code = new List<string>();
+        private List<string> language = new List<string>();
+        public string[] data_array = {
+                                      "Language Code",//0
+                                      "Language name",//1
+                                      "select language",//2
+        
+        };
+        public bool insert_language(string input01, string input02, out string output)
+        {
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].CommandType = CommandType.StoredProcedure;
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].Parameters.AddWithValue("@code", input01);
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].Parameters.AddWithValue("@language", input02);
+            int rowsAffected = Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].ExecuteNonQuery();
+
+            if (rowsAffected > 0)
+            {
+                output = "sucess";
+                status = true;
+            }
+
+            else
+            {
+               output = "fail";
+            status= false;      
+            }
+
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return status;
+        }
+        public bool find_code_using_code(string input, out string output)
+        {
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_code].CommandType = CommandType.StoredProcedure;
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_code].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_code].Parameters.AddWithValue("@code", input);
+            using (SqlDataReader reader = Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_code].ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    output = $"{reader["code"].ToString()}";
+                    status = true;
+
+                }
+                else
+                {
+                    output = "cant find";
+                    status = false;
+                }
+            }
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return status;
+        }
+        public bool find_code_using_language(string input, out string output)
+        {
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_language].CommandType = CommandType.StoredProcedure;
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_language].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_language].Parameters.AddWithValue("@language", input);
+            using (SqlDataReader reader = Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_code_using_language].ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    output = $"{reader["code"].ToString()}";
+                    status = true;
+                }
+                else
+                {
+                    output = "cant find";
+                    status = true;
+                }
+            }
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return status;
+        }
+        public bool find_language_using_code(string input, out string output)
+        {
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_code].CommandType = CommandType.StoredProcedure;
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_code].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_code].Parameters.AddWithValue("@code", input);
+            using (SqlDataReader reader = Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    output = $"{reader["language"].ToString()}";
+                    status = true;
+                }
+                else
+                {
+                    output = "cant find";
+                    status = false;
+                }
+            }
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return status;
+        }
+        public bool find_language_using_Language(string input, out string output)
+        {
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_Language].CommandType = CommandType.StoredProcedure;
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_Language].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.find_language_using_Language].Parameters.AddWithValue("@language", input);
+            using (SqlDataReader reader = Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.insert_language].ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    output = $"{reader["language"].ToString()}";
+                    status = true;
+                }
+                else
+                {
+                    output = "cant find";
+                    status = false;
+                }
+            }
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return status;
+        }
+        public string view_all_data_from_language_table()
+        {
+
+
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Open();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.view_all_data_from_language_table].Parameters.Clear();
+            Sql_Manager01.cmd[(int)Sql_Manager01.command_strings.view_all_data_from_language_table].CommandType = CommandType.StoredProcedure;
+            using (SqlDataReader reader = Sql_Manager01.cmd[22].ExecuteReader())
+            {
+                while (reader.Read())
+                {
+
+                    data01[0] += $"{reader["code"]}\n" +
+                                 $"{reader["language"]}\n";
+                    code.Add(reader["code"]?.ToString() ?? string.Empty);
+                    language.Add(reader["language"]?.ToString() ?? string.Empty);
+                }
+            }
+            data01[1] += $"{string.Join(" ", code)}\n" +
+                         $"{string.Join(" ", language)}\n";
+
+            Sql_Manager01.conn[(int)Sql_Manager01.Connection_strings.Connection01].Close();
+            return data01[0];
+
+        }
+
+    }
+}
