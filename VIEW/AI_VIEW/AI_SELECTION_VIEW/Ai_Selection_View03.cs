0b2a6f96671326c4e29cbb2fea8bc8fdf3efb6f7
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-21 02:24:03 -0700

    Add files via upload

---
+++ VIEW/AI_VIEW/AI_SELECTION_VIEW/Ai_Selection_View03.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_APP.VIEW.AI_VIEW.AI_SELECTION_VIEW
+{
+    internal class Ai_Selection_View03
+    {
+    }
+}
---
+++ VIEW/USER_VIEW/USER_SELECTION_VIEW/User_Selection_View01.cs
@@ -0,0 +1,66 @@
+﻿using E_APP.SERVICES.SECURITY_SERVICES;
+using E_APP.VIEW.USER_VIEW.USER_MAIN_VIEW;
+
+
+namespace E_APP.VIEW.USER_VIEW.USER_SELECTION_VIEW
+{
+    internal class User_Selection_View01
+    {
+        private static string[] data01 = new string[100];
+        private static Security_Services01 Security_Serv01 = new Security_Services01();
+
+        public User_Selection_View01()
+        {
+            load_User_Selection_View01();
+        }
+        private void load_User_Selection_View01()
+        {
+
+            data01[0] = $"1.) Login \n" +
+                         $"2.) go back";
+            Console.WriteLine(data01[0]);
+            data01[1] = Console.ReadLine() ?? string.Empty;
+
+            while (true)
+            {
+                if (Security_Serv01.empty_string(data01[1]) == true)
+                {
+                    if (Security_Serv01.string_only_digit(data01[1]) == true)
+                    {
+                        switch (int.Parse(data01[1]))
+                        {
+                            case 1:
+
+                                return;
+                            case 2:
+                                new User_Main_View01();
+                                return;
+                     
+
+
+                        }
+                    }
+                    else
+                    {
+
+
+                        data01[2] = "Input must be only digits. Please try again.";
+                        Console.WriteLine(data01[2]);
+                        data01[1] = Console.ReadLine() ?? string.Empty;
+                        continue;   
+                    }
+                }
+                else
+                {
+                    data01[2] = "Input cannot be empty. Please try again.";
+                    Console.WriteLine(data01[2]);
+                    data01[1] = Console.ReadLine() ?? string.Empty;
+                    continue;
+
+                }
+
+            }
+        }
+
+    }
+}
