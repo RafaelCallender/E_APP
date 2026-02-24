393819dd9341812b4d91f9cf441267cf54136aba
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-23 20:19:59 -0700

    Add files via upload

---
+++ SERVICES/PYTHON_SERVICES/AI_SERVICES/AI_TEXT_TO_IMAGE/Ai_Text_To_Image02.py
@@ -0,0 +1,47 @@
+﻿﻿import torch
+import sys
+from diffusers import ZImagePipeline
+from io import BytesIO
+import base64
+
+# -------------------------------------------------
+# Device & dtype
+# -------------------------------------------------
+device = "cuda" if torch.cuda.is_available() else "cpu"
+dtype = torch.float16 if device == "cuda" else torch.float32
+
+# -------------------------------------------------
+# Load model
+# -------------------------------------------------
+pipe = ZImagePipeline.from_pretrained(
+    "Tongyi-MAI/Z-Image-Turbo",
+    torch_dtype=dtype
+).to(device)
+
+# -------------------------------------------------
+# Get prompt from C#
+# -------------------------------------------------
+prompt = sys.argv[1] if len(sys.argv) > 1 else "The coolest robot ever, ultra detailed"
+
+# -------------------------------------------------
+# Generate image
+# -------------------------------------------------
+image = pipe(
+    prompt=prompt,
+    height=768,
+    width=768,
+    guidance_scale=5.0,
+    num_inference_steps=25
+).images[0]
+
+# -------------------------------------------------
+# Convert image to Base64
+# -------------------------------------------------
+buffer = BytesIO()
+image.save(buffer, format="PNG")
+encoded = base64.b64encode(buffer.getvalue()).decode()
+
+# -------------------------------------------------
+# Return Base64 string
+# -------------------------------------------------
+print(encoded)
---
+++ SERVICES/SQLITE/SQLITE_MANAGER/SQLITE_LANGUAGE_MANGER/Sqlite_Lanaguge_Manager01.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E_APP.SERVICES.SQLITE.SQLITE_MANAGER.SQL_LANGUAGE_MANGER
+{
+    internal class Sqlite_Lanaguge_Manager01
+    {
+    }
+}
