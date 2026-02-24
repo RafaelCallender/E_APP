94d2c7b3759167228cd57de989111fa8d49e62ce
E_CALLENDER <60360447+RafaelCallender@users.noreply.github.com>
2026-02-23 20:17:52 -0700

    Delete MODEL directory

--- MODEL/AI_MODEL/AI_IMAGE_MODEL_EDIT/SET_MODEL/Set_Model01.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace E_APP.MODEL.AI_MODEL.AI_IMAGE_MODEL_EDIT.AI_IMAGE_SET_MODEL
-{
-    internal class Set_Model01
-    {
-        public string image_url { get; set; }=string.Empty;
-        public string status { get; set; } = string.Empty;
-    }
-}
--- MODEL/LANGUAGE_MODEL/LANGUAGE_SET_MODEL/Language_Set_Model02.cs
+++
@@ -1,9 +0,0 @@
-﻿
-
-namespace E_APP.MODEL.LANGUAGE_MODEL.LANGUAGE_SET_MODEL
-{
-    internal class Language_Set_Model02
-    {
-        public List<string> trans { get; set; }= new List<string>();    
-    }
-}
